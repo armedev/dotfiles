0cebbca66e9e64b3adab5a189e3a642f6df536e7
Abdul Hameed <abdul.hameed@gida.io>
2024-02-27 15:30:25 +0530

    Added startup ascii art

--- nvim-custom/chadrc.lua
+++ nvim-custom/chadrc.lua
@@ -1,6 +1,30 @@
 ---@type ChadrcConfig 
 local M = {}
-M.ui = {theme = 'catppuccin'}
+M.ui = {
+  theme = 'catppuccin',
+ nvdash = {
+    load_on_startup = true,
+
+    header = {
+    [[       ███╗   ██╗██╗   ██╗██╗███╗   ███╗       ]],
+    [[       ████╗  ██║██║   ██║██║████╗ ████║       ]],
+    [[       ██╔██╗ ██║██║   ██║██║██╔████╔██║       ]],
+    [[       ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║       ]],
+    [[       ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║       ]],
+    [[       ══╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝       ]]
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+      { "Quit", ":q", ":q" },
+    },
+  },
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
