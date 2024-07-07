59ef142d78460e51f85e912df3d9430aba5a9d11
Arbaz Ahmed <51921579+armedev@users.noreply.github.com>
2024-07-07 20:33:38 +0530

    feat(nvim): added go lsp and plugins (#15)

--- nvim-custom/configs/lspconfig.lua
+++ nvim-custom/configs/lspconfig.lua
@@ -3,7 +3,6 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-
 lspconfig.rust_analyzer.setup({
   on_attach= on_attach,
   capabilities= capabilities,
@@ -17,6 +16,23 @@ lspconfig.rust_analyzer.setup({
     }
   }
 })
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
 
 lspconfig.tsserver.setup {
   on_attach = on_attach,
--- nvim-custom/plugins.lua
+++ nvim-custom/plugins.lua
@@ -1,45 +1,59 @@
 local plugins = {
-    {
-      "neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
 
-      dependencies = {
-       "jose-elias-alvarez/null-ls.nvim",
-       config = function()
-         require "custom.configs.null-ls"
-       end,
-     },
+    dependencies = {
+     "jose-elias-alvarez/null-ls.nvim",
+     config = function()
+       require "custom.configs.null-ls"
+     end,
+   },
 
-      config = function ()
-        require "plugins.configs.lspconfig"
-        require "custom.configs.lspconfig"
-      end,
-    },
-    {
-      "rust-lang/rust.vim",
-      ft = "rust",
-      init = function ()
-        vim.g.rustfmt_autosave = 1
-      end
-    },
-    {
-      "christoomey/vim-tmux-navigator",
-      lazy = false
-    },-- Lua
-    {
-      "nvim-telescope/telescope.nvim",
-      opts = require("custom.configs.telescope")
-    },
-    {
-      "ThePrimeagen/harpoon",
-      branch = "harpoon2",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function ()
-        require("custom.configs.harpoon")
-      end
-    },
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },-- Lua
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = require("custom.configs.telescope")
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      require("custom.configs.harpoon")
+    end
+  },
   {
     "tpope/vim-fugitive",
     lazy = false
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   }
  }
 
