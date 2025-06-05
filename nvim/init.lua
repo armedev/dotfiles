123e31a4e20c3f57bd01c8828e077e0eb6bdfce6
armedev <epiratesdev@gmail.com>
2025-06-05 16:45:15 +0530

    feat: some refactor

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require 'keymaps'
+-- options first
 require 'options'
 require 'aucommands'
 local plugins = require 'plugins'
@@ -37,6 +37,8 @@ require('lazy').setup(plugins, {
     },
   },
 })
+-- keymaps last
+require 'keymaps'
 
 -- The line beneath this is called `modeline` which overrides file processing for this file. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
